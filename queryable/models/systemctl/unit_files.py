ae7bec93dd7933791025fc45df89b2b1df911ca6
Christopher Sams <csams@redhat.com>
2020-05-09 15:11:47 -0500

    Models for systemctl list-units / list-unit-files
    Signed-off-by: Christopher Sams <csams@redhat.com>

---
+++ queryable/models/systemctl/unit_files.py
@@ -0,0 +1,23 @@
+from subprocess import check_output
+
+from queryable import Queryable
+
+
+def parse(content):
+    columns = ["unit_file", "state"]
+    results = []
+    for line in content[1:]:
+        line = line.strip()
+        if not line:
+            break
+        row = dict(zip(columns, line.split(None, 1)))
+        results.append(row)
+    return results
+
+
+def load():
+    return check_output(["systemctl", "list-unit-files"], encoding="utf-8").splitlines()
+
+
+def get():
+    return Queryable(parse(load()))
