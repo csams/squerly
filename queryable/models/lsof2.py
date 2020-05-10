fded4cc5bbcb8746f230062324947f6530b21905
Christopher Sams <csams@redhat.com>
2020-05-10 10:56:46 -0500

    lsof2 parsing optimizations.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- queryable/models/lsof2.py
+++ queryable/models/lsof2.py
@@ -6,53 +6,53 @@ from queryable import Dict, List, Queryable
 log = logging.getLogger(__name__)
 
 _table = {
-    "a": {"name": "access_mode", "type": str},
-    "c": {"name": "command", "type": str},
-    "C": {"name": "structure_share_count", "type": int},
-    "d": {"name": "device_character_code", "type": str},
-    "D": {"name": "device_number", "type": str},
-    "f": {"name": "descriptor", "type": str},
-    "F": {"name": "structure_address", "type": str},
-    "G": {"name": "flags", "type": str},
-    "g": {"name": "gid", "type": int},
-    "i": {"name": "inode_number", "type": int},
-    "K": {"name": "task_id", "type": int},
-    "k": {"name": "link_count", "type": int},
-    "l": {"name": "lock_status", "type": str},
-    "L": {"name": "login_name", "type": str},
-    "m": {"name": "repeated_output_marker", "type": str},
-    "M": {"name": "task_command", "type": str},
-    "n": {"name": "file_name", "type": str},
-    "N": {"name": "node_identifier", "type": str},
-    "o": {"name": "offset", "type": str},
-    "p": {"name": "pid", "type": int},
-    "P": {"name": "protocol_name", "type": str},
-    "r": {"name": "raw_device_number", "type": str},
-    "R": {"name": "ppid", "type": int},
-    "s": {"name": "size", "type": int},
-    "S": {"name": "stream", "type": str},
-    "t": {"name": "type", "type": str},
-    "TQR": {"name": "tcp_read_queue_size", "type": int},
-    "TQS": {"name": "tcp_send_queue_size", "type": int},
-    "TSO": {"name": "tcp_socket_options", "type": str},
-    "TSS": {"name": "tcp_socket_states", "type": str},
-    "TST": {"name": "tcp_connection_state", "type": str},
-    "TTF": {"name": "tcp_flags", "type": str},
-    "TWR": {"name": "tcp_window_read_size", "type": int},
-    "TWS": {"name": "tcp_window_write_size", "type": int},
-    "u": {"name": "uid", "type": int},
-    "z": {"name": "zone_name", "type": str},
-    "Z": {"name": "selinux_security_context", "type": str},
-    "0": {"name": "use_nul_sep", "type": str},
-    "1": {"name": "dialect_specific_1", "type": str},
-    "2": {"name": "dialect_specific_2", "type": str},
-    "3": {"name": "dialect_specific_3", "type": str},
-    "4": {"name": "dialect_specific_4", "type": str},
-    "5": {"name": "dialect_specific_5", "type": str},
-    "6": {"name": "dialect_specific_6", "type": str},
-    "7": {"name": "dialect_specific_7", "type": str},
-    "8": {"name": "dialect_specific_8", "type": str},
-    "9": {"name": "dialect_specific_9", "type": str},
+    "a": ("access_mode", str),
+    "c": ("command", str),
+    "C": ("structure_share_count", int),
+    "d": ("device_character_code", str),
+    "D": ("device_number", str),
+    "f": ("descriptor", str),
+    "F": ("structure_address", str),
+    "G": ("flags", str),
+    "g": ("gid", int),
+    "i": ("inode_number", int),
+    "K": ("task_id", int),
+    "k": ("link_count", int),
+    "l": ("lock_status", str),
+    "L": ("login_name", str),
+    "m": ("repeated_output_marker", str),
+    "M": ("task_command", str),
+    "n": ("file_name", str),
+    "N": ("node_identifier", str),
+    "o": ("offset", str),
+    "p": ("pid", int),
+    "P": ("protocol_name", str),
+    "r": ("raw_device_number", str),
+    "R": ("ppid", int),
+    "s": ("size", int),
+    "S": ("stream", str),
+    "t": ("type", str),
+    "TQR": ("tcp_read_queue_size", int),
+    "TQS": ("tcp_send_queue_size", int),
+    "TSO": ("tcp_socket_options", str),
+    "TSS": ("tcp_socket_states", str),
+    "TST": ("tcp_connection_state", str),
+    "TTF": ("tcp_flags", str),
+    "TWR": ("tcp_window_read_size", int),
+    "TWS": ("tcp_window_write_size", int),
+    "u": ("uid", int),
+    "z": ("zone_name", str),
+    "Z": ("selinux_security_context", str),
+    "0": ("use_nul_sep", str),
+    "1": ("dialect_specific_1", str),
+    "2": ("dialect_specific_2", str),
+    "3": ("dialect_specific_3", str),
+    "4": ("dialect_specific_4", str),
+    "5": ("dialect_specific_5", str),
+    "6": ("dialect_specific_6", str),
+    "7": ("dialect_specific_7", str),
+    "8": ("dialect_specific_8", str),
+    "9": ("dialect_specific_9", str),
 }
 
 
@@ -63,29 +63,27 @@ def parse(content):
     for line in content:
         line = line.rstrip()
 
-        if line.startswith("T"):
+        if line[0] == "T":
             k, v = line.split("=", 1)
             net = True
         else:
             k, v = line[0], line[1:]
 
-        meta = _table.get(k)
-        if not meta:
+        try:
+            key, _type = _table[k]
+            # start a new record
+            if key in one:
+                # overloaded prefix
+                if net and "file_name" in one:
+                    one["internet_address"] = one["file_name"]
+                    del one["file_name"]
+                results.append(one)
+                one = Dict(parents=[results])
+                net = False
+
+            one[key] = _type(v) if v != "" else None
+        except KeyError:
             log.warn(f"Skipping unknown key: %s", k)
-            continue
-
-        key, _type = meta["name"], meta["type"]
-        # start a new record
-        if key in one:
-            # overloaded prefix
-            if net and "file_name" in one:
-                one["internet_address"] = one["file_name"]
-                del one["file_name"]
-            results.append(one)
-            one = Dict(parents=[results])
-            net = False
-
-        one[key] = _type(v) if v != "" else None
     if one:
         results.append(one)
 
