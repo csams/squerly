d6ebf48978c48bf13b59facda38a64dcf6e766e9
Christopher Sams <csams@redhat.com>
2020-05-25 07:20:26 -0500

    Remove analyze from driver.py
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- driver.py
+++ driver.py
@@ -4,4 +4,4 @@ import squerly  # noqa
 from squerly.models import df, lsof, lsof2, meminfo, ps, rpms  # noqa
 from squerly import *  # noqa
 
-__all__ = ["df", "lsof", "lsof2", "meminfo", "ps", "rpms", "analyze"] + squerly.__all__
+__all__ = ["df", "lsof", "lsof2", "meminfo", "ps", "rpms"] + squerly.__all__
