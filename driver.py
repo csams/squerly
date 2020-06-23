8d58c53ad1c4d1b12ef4f1f98eea401f47f00ed8
Christopher Sams <csams@redhat.com>
2020-06-23 12:05:57 -0500

    Remove models from master.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- driver.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import squerly  # noqa
-from squerly.models import df, lsof, lsof2, meminfo, ps, rpms  # noqa
-from squerly import *  # noqa
-
-__all__ = ["df", "lsof", "lsof2", "meminfo", "ps", "rpms"] + squerly.__all__
